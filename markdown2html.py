574083340f38af4ac75c98935217d98b4522721f
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-16 02:51:53 +0300

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-'''Markdown to HTML'''
+''' Write a script markdown2html.py that takes an argument 2 strings:
+
+    First argument is the name of the Markdown file
+    Second argument is the output file name
+'''
 
 import sys
-import os.path as path
+import os.path
 
 
-if __name__ == "__main__":
-    '''Check if the number of arguments is correct'''
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py README.md README.html",
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html',
               file=sys.stderr)
-        sys.exit(1)
+        exit(1)
 
-    input_file = sys.argv[1]
-
-    if not path.exists(input_file):
-        print(f"Missing {input_file}", file=sys.stderr)
-        sys.exit(1)
-    sys.exit(0)
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
