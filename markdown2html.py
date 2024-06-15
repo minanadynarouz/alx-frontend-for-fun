1f8b0e37c38434ec3829dc432faaab8cf3fc3610
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-15 20:06:40 +0300

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 ''' Markdown to HTML '''
-from os import path
+
+
 import sys
+import os.path as path
+
+
+def main():
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not path.exists(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
+
 
 if __name__ == "__main__":
-    '''Check the file and input validity'''
-    if len(sys.argv) != 3:
-        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-        exit(1)
-
-    if not path.exists(sys.argv[1]):
-        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-        exit(1)
+    main()
