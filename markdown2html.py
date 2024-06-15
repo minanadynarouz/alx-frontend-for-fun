ebabcfada0a65fc1982201b43801d78943198eb6
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-16 02:47:04 +0300

    Task 0 & 1

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-''' Markdown to HTML '''
-
+'''Markdown to HTML'''
 
 import sys
 import os.path as path
@@ -14,17 +13,18 @@ def convert_to_html(in_file, out_file):
     with open(in_file, "r") as f:
         lines = f.readlines()
         for line in lines:
-            if (line.startswith('#')):
-                level = line.count('#')
-                hash_index = line.rindex("#")
-                line = line[hash_index + 1:].strip()
-                html.append(f"<h{level}>{line}</h{level}>\n")
-                with open(out_file, "w") as f:
-                    f.write(''.join(html))
+            line = line.rstrip()
+            if line.startswith('#'):
+                level = len(line.split(' ')[0])
+                header_content = line[level:].strip()
+                html.append(f"<h{level}>{header_content}</h{level}>\n")
+
+    with open(out_file, "w") as f:
+        f.write(''.join(html))
 
 
 def main():
-    ''' Check if the number of arguments is correct '''
+    '''Check if the number of arguments is correct'''
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
@@ -38,7 +38,6 @@ def main():
         sys.exit(1)
 
     convert_to_html(input_file, output_file)
-
     sys.exit(0)
 
 
