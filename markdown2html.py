c459ecd6af3577d95110861dd41d81b54a637034
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-16 02:50:47 +0300

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -5,7 +5,7 @@ import sys
 import os.path as path
 
 
-def main():
+if __name__ == "__main__":
     '''Check if the number of arguments is correct'''
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html",
@@ -13,13 +13,8 @@ def main():
         sys.exit(1)
 
     input_file = sys.argv[1]
-    output_file = sys.argv[2]
 
     if not path.exists(input_file):
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
     sys.exit(0)
-
-
-if __name__ == "__main__":
-    main()
