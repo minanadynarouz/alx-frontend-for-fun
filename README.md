10cfd4e2a9269e659965f99b1a5351191f99475b
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-16 01:08:55 +0300

    Task 1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Frontend for Fun
 Making a reader for Markdown files
+
+# My title
+## My title2
+# My title3
+#### My title4
+### My title5
--- markdown2html.py
+++ markdown2html.py
@@ -6,6 +6,23 @@ import sys
 import os.path as path
 
 
+def convert_to_html(in_file, out_file):
+    '''
+    Convert markdown to HTML
+    '''
+    html = []
+    with open(in_file, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if (line.startswith('#')):
+                level = line.count('#')
+                hash_index = line.rindex("#")
+                line = line[hash_index + 1:].strip()
+                html.append(f"<h{level}>{line}</h{level}>\n")
+                with open(out_file, "w") as f:
+                    f.write(''.join(html))
+
+
 def main():
     # Check if the number of arguments is correct
     if len(sys.argv) != 3:
@@ -18,6 +35,8 @@ def main():
     if not path.exists(input_file):
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
+
+    convert_to_html(input_file, output_file)
 
     sys.exit(0)
 
