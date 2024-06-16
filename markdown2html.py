13e1ee0d4917fa791d623c2df57704a586b1a16d
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-16 10:07:39 +0300

    Amend task 0

--- markdown2html.py
+++ markdown2html.py
@@ -9,13 +9,13 @@ import sys
 import os.path
 
 if __name__ == '__main__':
-	if len(sys.argv) < 3:
-        	print('Usage: ./markdown2html.py README.md README.html',
-              		file=sys.stderr)
-        	exit(1)
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        exit(1)
 
-    	if not os.path.isfile(sys.argv[1]):
-        	print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-        	exit(1)
-	
-	exit (0)	
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
+
+    exit(0)
