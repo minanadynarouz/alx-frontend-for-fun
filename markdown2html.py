bd075cb9a8ee670040a09c15e9c6e679b44ca40e
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-17 01:35:54 +0300

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python3
-''' Markdown to HTML '''
+
+"""
+A script to convert a Markdown file to an HTML file.
+"""
+
 import sys
-from os import path
+import os
+import markdown
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    html_file = sys.argv[2]
+
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    with open(markdown_file, 'r', encoding='utf-8') as f:
+        markdown_content = f.read()
+
+    html_content = markdown.markdown(markdown_content)
+
+    with open(html_file, 'w', encoding='utf-8') as f:
+        f.write(html_content)
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
-    '''the number of arguments is less than 2'''
-    if len(sys.argv) != 3:
-        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-        exit(1)
-    '''Markdown file doesn’t exist'''
-    if not path.exists(sys.argv[1]):
-        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-        exit(1)
-
-    exit(0)
+    main()
