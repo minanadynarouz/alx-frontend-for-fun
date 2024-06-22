d76ae223ab5d1653bdf175042b60105ad6736a31
root <root@MinaNady>
2024-06-22 16:13:12 +0300

    amend

--- markdown2html.py
+++ markdown2html.py
@@ -13,9 +13,10 @@ Example:
     ./markdown2html.py README.md README.html
 """
 
-import argparse
 import pathlib
 import re
+import sys
+import string
 
 
 def convert_md_to_html(input_file, output_file):
@@ -29,14 +30,12 @@ def convert_md_to_html(input_file, output_file):
     html_content = []
     for line in md_content:
         # Check if the line is a heading
-        match = re.match(r'(#){1,6} (.*)', line)
-        if match:
-            # Get the level of the heading
-            h_level = len(match.group(1))
-            # Get the content of the heading
-            h_content = match.group(2)
+        hashes = re.findall(r'#', line)
+        h_content = re.findall(r'\w', line)
+        if hashes:
+            h_level = len(hashes)
             # Append the HTML equivalent of the heading
-            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
+            html_content.append(f'<h{h_level}>{"".join(h_content)}</h{h_level}>\n')
         else:
             html_content.append(line)
 
@@ -46,17 +45,15 @@ def convert_md_to_html(input_file, output_file):
 
 
 if __name__ == '__main__':
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
-    parser.add_argument('input_file', help='path to input markdown file')
-    parser.add_argument('output_file', help='path to output HTML file')
-    args = parser.parse_args()
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
     # Check if the input file exists
-    input_path = pathlib.Path(args.input_file)
+    input_path = pathlib.Path(input_file)
     if not input_path.is_file():
         print(f'Missing {input_path}', file=sys.stderr)
         sys.exit(1)
 
     # Convert the markdown file to HTML
-    convert_md_to_html(args.input_file, args.output_file)
+    convert_md_to_html(input_file, output_file)
+
