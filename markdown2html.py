5f1b3fb340f2636f60116ca267f671c113756ae0
root <root@MinaNady>
2024-06-22 16:26:10 +0300

    a

--- markdown2html.py
+++ markdown2html.py
@@ -40,6 +40,11 @@ def convert_to_html(in_file, out_file):
 
 
 if __name__ == '__main__':
+
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+        sys.exit(1)
+
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
